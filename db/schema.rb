452b2e9cfe7ad74c6848ee949003780e0602644c
Jeongmin Byun <jmbyun91@gmail.com>
2017-02-09 01:14:14 +0900

    feat(package): allow CORS, limit API access, and implement token auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170120074216) do
+ActiveRecord::Schema.define(version: 20170208153546) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "groupname"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20170120074216) do
     t.integer  "group_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "api_key"
     t.index ["group_id"], name: "index_users_on_group_id"
   end
 
