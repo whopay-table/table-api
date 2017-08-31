ae317569d2af125a5ada7780aa44b04aee37890f
Jeongmin Byun <jmbyun91@gmail.com>
2017-08-31 11:40:25 +0900

    feat(package): add no-cache option to docker image publishing script

--- deploy-scripts/publish-docker-image.sh
+++ deploy-scripts/publish-docker-image.sh
@@ -1,3 +1,4 @@
-docker build -f Dockerfile.prod -t "table-api:$1" .
+echo "Building table-api:$1"
+docker build --no-cache -f Dockerfile.prod -t "table-api:$1" .
 docker tag "table-api:$1" "jmbyun/table-api:$1"
 docker push "jmbyun/table-api:$1"
