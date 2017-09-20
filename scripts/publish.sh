19a849e519f8adc58fc9e7718d165fc843bf3628
Jeongmin Byun <jmbyun91@gmail.com>
2017-09-21 07:27:30 +0900

    feat(package): implement publish script to push tag to git server

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
-
-if [ x$(git rev-parse --abbrev-ref HEAD | git rev-parse $(cat -)) = x$(git rev-parse --abbrev-ref HEAD | git rev-parse origin/$(cat -)) ]
+if [ x$(git rev-parse --abbrev-ref HEAD) != xmaster ]
 then
-    echo same
+    echo "Error: Cannot publish non-master branch."
+    exit 1
 fi
+
+if [ x$(git diff --exit-code) != x ]
+then
+    echo "Error: There are local unstaged changes in your code."
+    exit 1
+fi
+
+if [ x$(git diff --cached --exit-code) != x ]
+then
+    echo "Error: There are local uncommited changes in your code."
+    exit 1
+fi
+
+if [ x$()]
+
+if [ x$(git rev-parse master) != x$(git rev-parse origin/master) ]
+then
+    echo "Error: Local master branch is not synced with the remote master branch."
+    exit 1
+fi
+
+if [[ "$1" =~ "v[0-9\.]+" ]]
+then
+    echo "Publishing version $1"
+else
+    echo "Error: Version must be in v[0-9\.]+ form"
+fi
+
+git tag
