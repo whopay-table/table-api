fdebb513f867454839352a6efd3e567874b45e64
Jeongmin Byun <jmbyun91@gmail.com>
2017-09-21 12:06:12 +0900

    fix(package): fix publishing script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,25 +8,25 @@ then
     exit 1
 fi
 
-if [ x$(git diff --exit-code) != x ]
+if [ "x$(git diff --exit-code)" != "x" ]
 then
     echo "Error: There are local unstaged changes in your code."
     exit 1
 fi
 
-if [ x$(git diff --cached --exit-code) != x ]
+if [ "x$(git diff --cached --exit-code)" != "x" ]
 then
     echo "Error: There are local uncommited changes in your code."
     exit 1
 fi
 
-if [ x$(git rev-parse master) != x$(git rev-parse origin/master) ]
+if [ "x$(git rev-parse master)" != "x$(git rev-parse origin/master)" ]
 then
     echo "Error: Local master branch is not synced with the remote master branch."
     exit 1
 fi
 
-if [ $VERSION =~ v[0-9\.]+ ]
+if [[ $VERSION =~ v[0-9\.]+ ]]
 then
     echo "Tagging $VERSION on repository."
 else
