70250912e522da407b40cdae6f16c8883000c3c0
Jeongmin Byun <jmbyun91@gmail.com>
2017-09-21 12:26:00 +0900

    fix(package): fix install service

--- scripts/install-service.sh
+++ scripts/install-service.sh
@@ -1,3 +1,3 @@
 
-pwd | sed -e 's/\//\\\//g' | eval "sed -e 's/{{PWD}}/$(cat -)/g' ./scripts/table-web.service" > /etc/systemd/system/table-api.service
+pwd | sed -e 's/\//\\\//g' | eval "sed -e 's/{{PWD}}/$(cat -)/g' ./scripts/table-api.service" > /etc/systemd/system/table-api.service
 systemctl enable table-api
