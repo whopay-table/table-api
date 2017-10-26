ee91dc163247d36a2183bc0392140f6acd8a5fe3
Jeongmin Byun <jmbyun91@gmail.com>
2017-10-26 17:54:49 +0900

    feat(transaction): add auto_accept_at in serializer

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -1,5 +1,5 @@
 class TransactionSerializer < ActiveModel::Serializer
-  attributes :id, :amount, :description, :is_accepted, :is_rejected, :created_at
+  attributes :id, :amount, :description, :is_accepted, :is_rejected, :created_at, :auto_accept_at
   has_one :from_user
   has_one :to_user
 end
